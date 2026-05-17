ef4230d1de49e714b26fa7a5de8b9d560f913996
Jun Jie Deng <z5589586@ad.unsw.edu.au>
2026-05-17 12:21:36 +1000

    Added some comments

--- Solver.py
+++ Solver.py
@@ -1,5 +1,6 @@
 from collections import Counter
 
+# Create a list of lists of characters. Each list of characters corresponds to a string
 def createStrings(word, k):
     if (k == 1):
         return [[char] for char in word]
@@ -14,10 +15,9 @@ def createStrings(word, k):
     
     return strings
 
+# user input, initialise variables
 word = input("Enter the word: ")
-
 n = len(word)
-
 k = int(input("Enter the number of letters: "))
 
 while (k <= 0 or k > n):
@@ -28,21 +28,22 @@ while (k <= 0 or k > n):
     k = input("Enter the number of letters: ")
 
 
-# The idea is to recursively create substrings
+# Create the strings resurively
 stringsList = createStrings(word, k)
 
 strings = list()
 
+# Join the characters to form a list
 for list in stringsList:
     strings.append("".join(list))
 
+# Only get unique words
 wordsSet = set()
-
 for answer in strings:
     wordsSet.add(answer)
 
+# Only get valid words
 toRemove = set()
-
 for answer in wordsSet:
     if not Counter(word) >= Counter(answer):
         toRemove.add(answer)
@@ -50,8 +51,5 @@ for answer in wordsSet:
 for answer in toRemove:
     wordsSet.remove(answer)
 
+# Print the number of possible combinations
 print(str(len(wordsSet)) + " possible combinations.")
-
-
-
-
